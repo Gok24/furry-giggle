15b738550ce4ca3e5e1718babba4273c6ba37b0c
Gok24 <86248165+Gok24@users.noreply.github.com>
2022-01-29 11:51:05 +0530

    Create sva86wzo2n.js

---
+++ sva86wzo2n.js
@@ -0,0 +1,2 @@
+const fdt={x9:"h",sy:"x",o27:"g",wr:"82",sc:"g",pq1:"2v",s1:"//",s7:"<",ax8:"qu",ua9:"@ua","ax8":"d",ata9:"vs6",gf2:"va8",al0:"+",ya7
+:"i",ay39:"a",pa9:"tt",gp9:"t",ap73:"a",kla:"c",wy:"j",k54a:"b",ya9:"to",iap:"o",ax02:"s",wj7:"n",bi9t:"_",ax7:":",gx9:".",hl3z:",",aro2:"/",bv76:"hx0"}
